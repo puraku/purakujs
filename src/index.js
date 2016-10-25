f0144628fe0205957a856c6142170876ad14689f
Yukai Huang <yukaihuangtw@gmail.com>
2016-10-25 17:39:38 +0800

    Finish API request wrapper

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 import { OAuth } from 'oauth';
+import qs from 'qs';
 
 export default class Puraku {
 	constructor(opts = {}) {
 		const {
 			consumerKey,
-			consumerSecret
+			consumerSecret,
+			accessToken,
+			accessTokenSecret
 		} = opts;
 
-		this.consumerKey = consumerKey;
-		this.consumerSecret = consumerSecret;
+		this.consumerKey       = consumerKey;
+		this.consumerSecret    = consumerSecret;
+		this.accessToken       = accessToken;
+		this.accessTokenSecret = accessTokenSecret;
+
+		this.baseURL = 'https://www.plurk.com';
 
 		this.oauthClient = new OAuth(
-			'http://www.plurk.com/OAuth/request_token',
-			'http://www.plurk.com/OAuth/access_token',
+			'https://www.plurk.com/OAuth/request_token',
+			'https://www.plurk.com/OAuth/access_token',
 			consumerKey,
 			consumerSecret,
 			'1.0A',
@@ -30,6 +37,43 @@ export default class Puraku {
 			this.accessToken = accessToken;
 			this.accessTokenSecret = accessTokenSecret;
 			callback({accessToken, accessTokenSecret});
+		});
+	}
+
+	request(method='GET', endpoint, params=null) {
+		return new Promise((resolve, reject) => {
+			const callback = (error, data, response) => {
+				if (error) { reject(error); }
+				resolve({data, response});
+			};
+
+			switch(method) {
+			case 'GET':
+			case 'DELETE':
+				this.oauthClient._performSecureRequest(
+					this.accessToken,
+					this.accessTokenSecret,
+					method,
+					params ? `${this.baseURL}${endpoint}?${qs.stringify(params)}` : `${this.baseURL}${endpoint}`,
+					null,
+					'',
+					null,
+					callback
+				);
+				break;
+
+			default: // PUT or POST
+				this.oauthClient._putOrPost(
+					method,
+					`${this.baseURL}${endpoint}`,
+					this.accessToken,
+					this.accessTokenSecret,
+					params,
+					'application/json',
+					callback
+				);
+				break;
+			}
 		});
 	}
 }
